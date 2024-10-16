096db597446f0228f86da07524eeba2aa45d8f06
Zoey <probablyzoey.c@gmail.com>
2024-10-16 16:48:19 +0100

    Fixed a lot of chord related bugs
    Removed some now redundant checks in various conditional statements
    Improved readability:
    - renamed activeArray to pressedMidiKeys
    - renamed removeKeys to removeKeysFromArrays
    - renamed pressKeys to mimicKeyPresses
    
    Chord fixes:
    - changed triggeredChords.clear to triggeredChords.remove(code) to prevent issues when pressing and releasing multiple chords
    - added a check in removeKeysFromArrays to see if the current code is stored in the triggeredChords list
    
    Removed some code I'd left in by accident
    
    This might be the last big change to the midi handling, as I'm now really happy with it
    I could always make it a little better at handling different sizes of keyboard, but without having some keyboards to test it with, it's not high priority
    I'm sure more bugs will turn up at some point anyways ^^

--- MidiHandling.java
+++ MidiHandling.java
@@ -27,7 +27,7 @@ public class MidiHandling {
 	 */
 	private static final String transmitterName = "default";
 	private static final String transmitterProperties = "javax.sound.midi.Transmitter";
-	private static List<Integer> activeKeys = new CopyOnWriteArrayList<>();
+	private static List<Integer> pressedMidiKeys = new CopyOnWriteArrayList<>();
 	private static List<Integer> keysToRemove = new CopyOnWriteArrayList<>();
 	private static List<Integer> pressedKeys = new CopyOnWriteArrayList<>();
 	private static Robot robot;
@@ -112,9 +112,9 @@ public class MidiHandling {
 		public void send(MidiMessage message, long timeStamp) {
 			byte[] bytes = message.getMessage();
 			int key = byteToInt(bytes[1]);
-			// If the action detected is a keypress add it to activeKeys
-			if (bytes[2] == 100 && !activeKeys.contains(key)) {
-				activeKeys.add(key);
+			// If the action detected is a keypress add it to pressedMidiKeys
+			if (bytes[2] == 100) {
+				pressedMidiKeys.add(key);
 			}
 			// If the action detected is a key being raised then it is added to keysToRemove
 			else if (bytes[2] == 64) {
@@ -127,7 +127,7 @@ public class MidiHandling {
 		}
 
 		public static List<Integer> getActiveKeys() {
-			return activeKeys;
+			return pressedMidiKeys;
 		}
 
 		// When the program is exited the receiver is closed to free up resources
@@ -151,9 +151,11 @@ public class MidiHandling {
 
 		public void run() {
 			System.out.println("Starting thread...");
+			// This thread needs to permanently loop for as long as the program is running to give real time updates to key strokes
 			while (true) {
-				for (Integer n : activeKeys) {
-					pressKeys();
+				// For each key in the pressedMidiKeys list, it will mimic key presses for those items
+				for (Integer n : pressedMidiKeys) {
+					mimicKeyPresses();
 				};
 				try {
 					Thread.sleep(50); // Adjust the repeat rate as necessary, 50 seems to be more than responsive enough
@@ -163,27 +165,28 @@ public class MidiHandling {
 				}
 				// If there are any keys to remove it will run the appropriate function
 				if (!keysToRemove.isEmpty()) {
-					removeKeys();
+					removeKeysFromArrays();
 				}
 			}
 		}
 
 
 		// Function to find each key in the remove keys array, erase it from the main array and clear itself when its done iterating
-		private static void removeKeys() {
+		private static void removeKeysFromArrays() {
 			// For each key that is being removed
 			for (Integer keyNumber : keysToRemove) {
+				Object[] test = triggeredChords.toArray();
 				// Go through each key in the config
 				for (int i = 0; i < keyControls.length; i++) {
 					String currentCode = keyControls[i][0]; // currentCode is the current config line being looked at
-					if (containsKey(currentCode, keyNumber)) {
+					if (containsKey(currentCode, keyNumber) && triggeredChords.contains(currentCode)) {
 						int keyCode = Integer.valueOf(keyControls[i][1]);
 						keyHandling(keyCode, false);
-						triggeredChords.clear(); // I kinda just threw this here as a test but it kind of works??
+						triggeredChords.remove(currentCode);
 						break;
 					}
 				}
-				activeKeys.remove(keyNumber); // Remove the key from activeKeys
+				pressedMidiKeys.remove(keyNumber); // Remove the key from pressedMidiKeys
 				pressedKeys.remove(keyNumber); // Remove the key from pressedKeys
 				keysToRemove.remove(keyNumber); // Remove the key from keysToRemove
 			}
@@ -191,8 +194,8 @@ public class MidiHandling {
 
 
 		// The function responsible for mimicking the appropriate key presses
-		private static void pressKeys() {
-			for (Integer currentKey : activeKeys) { // For each currently pressed key
+		private static void mimicKeyPresses() {
+			for (Integer currentKey : pressedMidiKeys) { // For each currently pressed key
 				if (!pressedKeys.contains(currentKey)) {
 					for (int i = 0; i < keyControls.length; i++) { // Go through each item in the config
 
@@ -213,6 +216,7 @@ public class MidiHandling {
 
 
 		// Used to check if the keybind in config contains a key by checking it in pairs, as each key is 2 digits long
+		// This helps negate the chances of a false key, such as if a config had 5678 and you're testing for key 67, it would be a false positive
 		private static boolean containsKey(String currentCode, Integer keyNumber) {
 			for (int i = 0; i < currentCode.length(); i += 2) {
 				String pair = currentCode.substring(i, i + 2);
@@ -226,17 +230,17 @@ public class MidiHandling {
 
 		// Function to check if a chord is fully played and handle its triggering
 		private static boolean isCompleteChord(String chord, Integer startKey) {
-			String startKeyStr = startKey.toString();
-			String remainingChord = chord.substring(startKeyStr.length());
 
-			// Convert remaining part of the chord to individual keys contained in a new arraylist
-			List<Integer> remainingKeys = new ArrayList<>();
-			for (int i = 0; i < remainingChord.length(); i += 2) {
-				remainingKeys.add(Integer.parseInt(remainingChord.substring(i, i + 2)));
+			// Convert the code received from the config into its separate keys, and then checking if all of those keys are currently pressed
+			List<Integer> chordKeys = new ArrayList<>();
+			// Because each key on a standard 61 key keyboard is a 2 digit number, the code can be broken up into it
+			for (int i = 0; i < chord.length(); i += 2) {
+				// Each of these keys are then added to a list of chord keys
+				chordKeys.add(Integer.parseInt(chord.substring(i, i + 2)));
 			}
 
-			// Check if all remaining keys are currently active
-			return activeKeys.containsAll(remainingKeys);
+			// Returns true or false depending on if all chord keys are currently active
+			return pressedMidiKeys.containsAll(chordKeys);
 		}
 
 
@@ -247,6 +251,7 @@ public class MidiHandling {
 			if (keyCode >= 1000) {
 				if (isPress) {
 					robot.mousePress(keyCode);
+					System.out.println("Pressing");
 					mainGUI.insertText(getMouseNumber(keyCode) + " Pressed");
 					
 				} else {
