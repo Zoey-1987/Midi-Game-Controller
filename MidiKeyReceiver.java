634d66716c3c520a48384ab8d639c488b7e62c6f
Zoey <probablyzoey.c@gmail.com>
2025-09-22 17:06:06 +0100

    Add files via upload

--- MidiKeyReceiver.java
+++ MidiKeyReceiver.java
@@ -1,15 +1,22 @@
 package dissonance_midihandling;
 
 import java.util.List;
-
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.Receiver;
 
+/* -------------------------------------------------------
+ * 	This class uses the default receiver and overrides
+ * 	the send method to keep other classes up to date
+ * 	with what keys are currently pressed
+ ------------------------------------------------------- */
+
 public class MidiKeyReceiver implements Receiver {
     private final List<Integer> pressedKeys;
+    private final MidiKeyHandler handler;
 
-    public MidiKeyReceiver(List<Integer> pressedKeys) {
+    public MidiKeyReceiver(List<Integer> pressedKeys, MidiKeyHandler handler) {
         this.pressedKeys = pressedKeys;
+        this.handler = handler;
     }
 
     @Override
@@ -19,9 +26,11 @@ public class MidiKeyReceiver implements Receiver {
 
         if (data[2] == 100) {
             if (!pressedKeys.contains(key)) pressedKeys.add(key);
+            pressedKeys.sort(null);
         } else if (data[2] == 64) {
             pressedKeys.remove(Integer.valueOf(key));
         }
+        handler.notifyListeners();
     }
 
     @Override
